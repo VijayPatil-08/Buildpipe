0e1049fa54e6f8e75a70bb57f4b0806cc9b56142
VijayPatil-08 <vijuhy6428@gmail.com>
2021-03-30 11:47:42 +0530

    Commit pipe

---
+++ Models/MoviesRepository.cs
@@ -0,0 +1,45 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildPipeEditDockerProject.Models
+{
+    
+    public class MoviesRepository : IRepository<Movie>
+    {
+        public List<Movie> movies;
+
+       // private static readonly object forLock = new object();
+        
+       // private static MoviesRepository repository = null;
+
+        private MoviesRepository()
+        {
+            movies = new List<Movie>();
+            movies.Add(new Movie
+            {
+                Id = 1,
+                Name = "Bat-Men",
+                Durations = 12.3f
+            });
+        }
+
+        
+
+        public void AddItem(Movie movie)
+        {
+            this.movies.Add(movie);
+        }
+
+        public IEnumerable<Movie> GetItems()
+        {
+            return this.movies;
+        }
+        public Movie GetItemById(int id)
+        {
+            return this.movies.FirstOrDefault(m => m.Id == id);
+        }
+    }
+}
